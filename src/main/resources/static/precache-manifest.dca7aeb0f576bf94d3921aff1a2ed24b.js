8df7a9f9fcf72e3229ad60a796036734d3de67e5
Adrian Zawadzki <zawadzkii@tlen.pl>
2020-09-17 16:08:35 +0200

    add frontend

---
+++ src/main/resources/static/precache-manifest.dca7aeb0f576bf94d3921aff1a2ed24b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8713e812f533ec31c830a78699f90a18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82a11e836139936a30b1",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "fe658733992dc3767397",
+    "url": "/static/css/main.7556a164.chunk.css"
+  },
+  {
+    "revision": "82a11e836139936a30b1",
+    "url": "/static/js/2.27db2764.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.27db2764.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe658733992dc3767397",
+    "url": "/static/js/main.3b9b7bcf.chunk.js"
+  },
+  {
+    "revision": "f0395242fcfe40790f57",
+    "url": "/static/js/runtime-main.85b40700.js"
+  }
+]);
